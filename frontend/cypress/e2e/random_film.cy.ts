91249bf4ef805049c672ad069dad96c3c60d1680
Marina Papapetrou <contact@marinapapap.com>
2023-04-03 14:09:06 +0100

    Passed e2e test that checks rendered elements are populated with the correct information.

--- frontend/cypress/e2e/random_film.cy.ts
+++ frontend/cypress/e2e/random_film.cy.ts
@@ -14,4 +14,35 @@ describe("Random Film", () => {
     cy.get('[data-cy="crew"]').should("be.visible");
     cy.get('[data-cy="rating"]').should("be.visible");
   });
+
+  it("when button is clicked, elements are rendered and populated with correct information", () => {
+    cy.intercept("GET", "/randomFilm", (req) => {
+      req.reply({
+        body: {
+          message: "ok",
+          result: {
+            id: "1",
+            rank: "1",
+            title: "Film 1",
+            fullTitle: "Film 1 (2007)",
+            year: "2007",
+            image: "img.jpeg",
+            crew: "Director",
+            imDbRating: "5.7",
+            imDbRatingCount: "100",
+          },
+        },
+      });
+    }).as("getFilm");
+
+    cy.visit("/");
+    cy.get('[data-cy="button"]').click();
+
+    cy.wait("@getFilm").then(() => {
+      cy.get('[data-cy="fulltitle"]').should("contain.text", "Film 1 (2007)");
+      cy.get('[data-cy="image"]').should("exist");
+      cy.get('[data-cy="crew"]').should("contain.text", "Director");
+      cy.get('[data-cy="rating"]').should("contain.text", "5.7");
+    });
+  });
 });
