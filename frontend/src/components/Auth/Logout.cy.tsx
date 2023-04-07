a75de0d95f3d3ca5c61566b3d7e647d9e29e8bd8
Marina Papapetrou <contact@marinapapap.com>
2023-04-07 20:32:01 +0100

    Passed test to ensure logout button isn't rendered if the user isn't logged in.

--- frontend/src/components/Auth/Logout.cy.tsx
+++ frontend/src/components/Auth/Logout.cy.tsx
@@ -3,11 +3,13 @@ import { LogoutForm } from "./Logout";
 const navigate = () => {};
 
 describe("Login", () => {
-  beforeEach(() => {
+  it("renders button", () => {
     cy.mount(<LogoutForm navigate={navigate} inSession={true} />);
+    cy.get('[data-cy="logout-button"]').should("be.visible");
   });
 
-  it("renders button", () => {
-    cy.get('[data-cy="logout-button"]').should("be.visible");
+  it("does not render button", () => {
+    cy.mount(<LogoutForm navigate={navigate} inSession={false} />);
+    cy.get('[data-cy="logout-button"]').should("not.exist");
   });
 });
