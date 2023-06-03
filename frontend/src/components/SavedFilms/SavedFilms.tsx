b70eb65c919c3e0071a4f5a478d33484885232a3
Marina Papapetrou <contact@marinapapap.com>
2023-06-03 22:58:01 +0100

    Saved Films are returned to the frontend on the first render of the component

--- frontend/src/components/SavedFilms/SavedFilms.tsx
+++ frontend/src/components/SavedFilms/SavedFilms.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "../../App.css";
-import { Logout } from "../Auth/Logout";
+// import { Logout } from "../Auth/Logout";
+
+// interface Saved {
+//   films: any[];
+// }
 
 interface SavedFilmsProps {
   navigate: Function;
@@ -11,25 +15,46 @@ export const SavedFilms: React.FC<SavedFilmsProps> = ({
   navigate,
   setGlobalSession,
 }) => {
-  const [savedFilms, setSavedFilms] = useState<any>({});
+  const [inSession, setInSession] = useState<boolean>(false);
+  const [saved, setSaved] = useState<any[]>([]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const validateToken = async () => {
       try {
         const response = await fetch("/tokens/validate");
         const responseData = await response.json();
-
-        console.log(responseData);
-        setSavedFilms(responseData);
+        if (!responseData.session) {
+          navigate("/");
+        }
+        setInSession(true);
       } catch (error) {
+        navigate("/");
         console.error(error);
       }
     };
 
-    fetchData();
+    const fetchFilms = async () => {
+      try {
+        const response = await fetch("/users/films");
+        const responseData = await response.json();
+        setSaved(responseData.films);
+      } catch (error) {
+        navigate("/");
+        console.error(error);
+      }
+    };
+
+    validateToken();
+    fetchFilms();
   }, []);
 
-  return null;
+  const renderFilm = (): JSX.Element[] => {
+    return saved.map((film: any) => {
+      return <div key={film.id}>{film.title}</div>;
+    });
+  };
+
+  return <>{renderFilm()}</>;
 };
 
 export default SavedFilms;
