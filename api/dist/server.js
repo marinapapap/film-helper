49c739b672ccdf809c0c95737b26b7ed40d2c3f2
Marina Papapetrou <contact@marinapapap.com>
2023-08-03 21:07:08 +0100

    Rate limit added to /randomFilm endpoint

--- api/dist/server.js
+++ api/dist/server.js
@@ -8,17 +8,25 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = require("dotenv");
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
 (0, dotenv_1.config)({ path: "./config.env" });
 exports.app = (0, express_1.default)();
+const apiLimiter = (0, express_rate_limit_1.default)({
+    windowMs: 15 * 60 * 1000,
+    max: 5,
+    standardHeaders: true,
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 exports.app.use((0, cors_1.default)({ credentials: true, origin: process.env.FRONT_END_URL }));
 exports.app.use((0, cookie_parser_1.default)());
 exports.app.use(express_1.default.json());
+exports.app.set("trust proxy", true);
 const randomFilm_1 = __importDefault(require("./routes/randomFilm"));
 const users_1 = __importDefault(require("./routes/users"));
 const tokens_1 = __importDefault(require("./routes/tokens"));
 const savedFilms_1 = __importDefault(require("./routes/savedFilms"));
 // routes
-exports.app.use("/randomFilm", randomFilm_1.default);
+exports.app.use("/randomFilm", apiLimiter, randomFilm_1.default);
 exports.app.use("/users", users_1.default);
 exports.app.use("/tokens", tokens_1.default);
 exports.app.use("/savedFilms", savedFilms_1.default);
--- api/src/server.ts
+++ api/src/server.ts
@@ -2,14 +2,24 @@ import express, { Express } from "express";
 import cors from "cors";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
 
 config({ path: "./config.env" });
 
 export const app: Express = express();
 
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 app.use(cors({ credentials: true, origin: process.env.FRONT_END_URL }));
 app.use(cookieParser());
 app.use(express.json());
+
+app.set("trust proxy", true);
 
 import randomFilmRouter from "./routes/randomFilm";
 import userRouter from "./routes/users";
@@ -18,7 +28,7 @@ import savedFilmsRouter from "./routes/savedFilms";
 
 // routes
 
-app.use("/randomFilm", randomFilmRouter);
+app.use("/randomFilm", apiLimiter, randomFilmRouter);
 app.use("/users", userRouter);
 app.use("/tokens", tokensRouter);
 app.use("/savedFilms", savedFilmsRouter);
